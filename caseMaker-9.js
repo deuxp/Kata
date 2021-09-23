d4573f59b5a161d659990044c8f776643141900e
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-09-23 16:29:28 -0400

    renamed files to fit a uniform naming convention

--- caseMaker-9.js
+++ caseMaker-9.js
@@ -2,20 +2,22 @@ const camelCase = function (input) {
 
 
   const whitespace = ' '; // whitespace identifier
-  let capsToggle = false; // siwtch on caps mode
+  let shiftIndex = 0; // manually shift index on whitespace detection
   let cameled = ''; // camel cased letter by letter; brutish
 
   for (letter = 0; letter < input.length; letter++) { // for each letter from the input
 
-    if (input[letter] === whitespace) { // initiate whitespace protocol followed by caps mode
-
+    if (input[letter + shiftIndex] === undefined) { // once end of sequence is met; return
+      return cameled
     }
-
-
-
+    if (input[letter + shiftIndex] === whitespace) { // initiate whitespace protocol
+      shiftIndex++; //  index shift
+      cameled += (input[letter + shiftIndex]).toUpperCase(); // bypass whitespace & force caps-> [letter + 1].toUppercase()
+    } else {
+      cameled += input[letter + shiftIndex];
+    }
   }
-
-
+  return input // default if no whitespace
 };
 
 console.log(camelCase("this is a string")); // thisIsAString
@@ -23,5 +25,4 @@ console.log(camelCase("loopy lighthouse")); // loopyLighthouse
 console.log(camelCase("supercalifragalisticexpialidocious")); // supercalifragalisticexpialidocious
 
 
-
-// time spent so far -> 00:39:00
+// took a bit longer to debug the first iteration
