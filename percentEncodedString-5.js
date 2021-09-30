1342ac31b5a349da2b6816150cb2079a683701b2
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-09-30 00:13:50 -0400

    updated kata 5

--- percentEncodedString-5.js
+++ percentEncodedString-5.js
@@ -1,16 +1,50 @@
-
+/* 
 const urlEncode = function (text) {
   let encodedWord = [];
-  let trimdword = text.trim();
-  for (let letter = 0; letter < trimdword.length; letter++) {
-    if (trimdword[letter] === ' ') {
+  let text = text.trim();
+  for (let letter = 0; letter < text.length; letter++) {
+    if (text[letter] === ' ') {
       encodedWord += '%20';
     } else {
-      encodedWord += trimdword[letter];
+      encodedWord += text[letter];
     }
   }
   return encodedWord
 };
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure")); */
+
+
+
+
+
+
+const urlEncode = function (text) {
+
+  let encodedWord = [];
+
+  // trims outer white space
+  while (text[0] === ' ' || text[text.length - 1] === ' ') {
+    if (text[0] === ' ') {
+      text = text.slice(1, text.length)
+    } else {
+      text = text.slice(0, text.length - 1)
+    }
+  }
+
+  // replaces the inner whitespace with encoding
+  for (let letter = 0; letter < text.length; letter++) {
+    if (text[letter] === ' ') {
+      encodedWord += '%20';
+    } else {
+      encodedWord += text[letter];
+    }
+  }
+  return encodedWord
+};
+
 
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode(" Lighthouse Labs "));
