d65f02ac74bbcab99d32c288c87f3a8f0f1e8bc5
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-05 15:36:58 -0400

    finished Organizing-instructors-15, tested ok

---
+++ Organizing-Instructors-15.js
@@ -0,0 +1,63 @@
+/* Create a function named organizeInstructors that will 
+receive an array of instructor objects, and 
+will return a new object that has the following format:
+
+
+{
+  CourseName: [instructors]
+}  */
+
+// Good Luck
+
+
+
+const organizeInstructors = function (instructors) {
+
+
+  const o = new Object;
+  let curriculum = []
+  /* first figure out how to iterate course values */
+  instructors.forEach(instructor => {
+    if (curriculum.indexOf(instructor.course) > -1) {  // course key already in o; push(instructor.name) to course value
+      o[instructor.course].push(instructor.name);
+
+    } else {
+      o[instructor.course] = [instructor.name];  // adds a new key: value pair to o 
+      curriculum.push(instructor.course);
+
+
+    }
+
+  });
+
+  return o
+
+};
+
+console.log(organizeInstructors([
+  { name: "Samuel", course: "iOS" },
+  { name: "Victoria", course: "Web" },
+  { name: "Karim", course: "Web" },
+  { name: "Donald", course: "Web" }
+]));
+
+/* {
+  iOS: ["Samuel"],
+  Web: ["Victoria", "Karim", "Donald"]
+} */
+
+
+console.log(organizeInstructors([
+  { name: "Brendan", course: "Blockchain" },
+  { name: "David", course: "Web" },
+  { name: "Martha", course: "iOS" },
+  { name: "Carlos", course: "Web" }
+]));
+
+/* {
+  Blockchain: ["Brendan"],
+  Web: ["David", "Carlos"],
+  iOS: ["Martha"]
+} */
+
+// 2hrs
--- caseMaker-9.js
+++ caseMaker-9.js
@@ -3,18 +3,18 @@ const camelCase = function (input) {
 
   const whitespace = ' '; // whitespace identifier
   let shiftIndex = 0; // manually shift index on whitespace detection
-  let cameled = ''; // camel cased letter by letter; brutish
+  let cameled = ''; // camel cased letter by letter;
 
   for (letter = 0; letter < input.length; letter++) { // for each letter from the input
 
-    if (input[letter + shiftIndex] === undefined) { // once end of sequence is met; return
+    if (input[letter + shiftIndex] === undefined) { // end case; return
       return cameled
     }
     if (input[letter + shiftIndex] === whitespace) { // initiate whitespace protocol
       shiftIndex++; //  index shift
       cameled += (input[letter + shiftIndex]).toUpperCase(); // bypass whitespace & force caps-> [letter + 1].toUppercase()
     } else {
-      cameled += input[letter + shiftIndex];
+      cameled += input[letter + shiftIndex]; // concat letter to cameled
     }
   }
   return input // default if no whitespace
