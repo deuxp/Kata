f3ebdab2e4b11f47b9e1f983ab62e4bc2c37d4eb
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-07 00:59:29 -0400

    intro v2 of casemakerII

--- Case-MakerII-16.js
+++ Case-MakerII-16.js
@@ -16,7 +16,12 @@ const makeCase = function (input, cases) {
   const skipSeparators = ' -_';
 
 
-
+  const precedence = [
+    ['camel', 'pascal', 'snake', 'kebab', 'title'],
+    ['vowel', 'consonant'],
+    ['upper', 'lower'],
+    { positon1: [], position2: [], position3: [] } // precedence[3]
+  ];
 
 
 
@@ -31,20 +36,20 @@ const makeCase = function (input, cases) {
       let n = 0;  // manual shift
 
       for (let i = 0; i < input.length; i++) {
-        let i = i + n
+        let index = i + n
 
         //base camel
-        if (input[i] === undefined) {
+        if (input[index] === undefined) {
           return processed.join('');
 
         } else {
 
-          if (input[i] === whitespace) {
-            processed[i] = input[i + 1].toUpperCase();
+          if (input[index] === whitespace) {
+            processed[index] = input[index + 1].toUpperCase();
             n++
 
           } else {
-            processed[i] = input[i];
+            processed[index] = input[index];
           }
         }
       }
@@ -110,7 +115,6 @@ const makeCase = function (input, cases) {
     // case 5: TITLE
 
     title: function (input) {
-
       processed[0] = input[0].toUpperCase();
 
       for (let i = 1; i < input.length; i++) {
@@ -126,22 +130,46 @@ const makeCase = function (input, cases) {
     // case 6: vowels
 
     vowels: function (input) {
-
-      // const skipSeparators = ' -_';
-
       for (let i = 0; i < input.length; i++) {
+        if (vowel.indexOf(input[i]) > -1) processed[i] = input[i].toUpperCase();
+        else processed[i] = input[i];
+      }
+      return processed.join('');
+    },
 
-        if (skipSeparators.indexOf(input[i] > -1)) { // in list
-          continue;
-
-        } else if {
-          // in the vowels list
-          // processed[i] = input[i];
+    consonants: function (input) {
+      for (let i = 0; i < input.length; i++) {
+        if (vowel.indexOf(input[i]) <= -1 && skipSeparators.indexOf(input[i]) <= -1) {
+          processed[i] = input[i].toUpperCase();
+        } else {
+          processed[i] = input[i];
         }
       }
       return processed.join('');
     },
 
+
+    // precedence 3:
+
+    // case: upper
+
+    upper: function (input) {
+      for (let i = 0; i < input.length; i++) {
+        if (skipSeparators.indexOf(input[i]) > -1) processed[i] = input[i];
+        else processed[i] = input[i].toUpperCase();
+      }
+      return processed.join('');
+    },
+
+    // case: lower
+
+    lower: function (input) {
+      for (let i = 0; i < input.length; i++) {
+        if (skipSeparators.indexOf(input[i]) > -1) processed[i] = input[i];
+        else processed[i] = input[i].tolowerCase();
+      }
+      return processed.join('');
+    },
 
   };
 
@@ -153,25 +181,33 @@ const makeCase = function (input, cases) {
     return input
   }
 
-  // set precedence: loop to temps; then reassign to same var so you dont alter the code.
+  // set precedence to the cases: loop to temps; then re-assign to same var so you dont alter the code.
+  cases = [cases]
+  let precedenceTemp = [];
+  for (let i = 0; i < cases.length; i++) {
+    const cas = cases[i];
+    if (precedence[0].indexOf(cas) > -1) precedence[3].positon1 = [cas];
+    else if (precedence[1].indexOf(cas) > -1) precedence[3].positon2 = [cas];
+    else precedence[3].positon3 = [cas];
+  }
+  cases = precedenceTemp.concat(precedence[3].positon1).concat(precedence[3].positon2).concat(precedence[3].positon3)
 
+  // insert key
+  const popCase = cases.splice(0, 1) // cases less one
+  let processedTemp = caseFilter[popCase[0]](input) // applied formatter
 
-
-
-
-
-
+  return makeCase(processedTemp, cases)
 }
 
 
 console.log(makeCase("this is a string", "camel")); // thisIsAString
-console.log(makeCase("this is a string", "pascal")); // ThisIsAString
-console.log(makeCase("this is a string", "snake")); // this_is_a_string
-console.log(makeCase("this is a string", "kebab")); // this-is-a-string
-console.log(makeCase("this is a string", "title")); // This Is A String
-console.log(makeCase("this is a string", "vowel")); // thIs Is A strIng
-console.log(makeCase("this is a string", "consonant")); // THiS iS a STRiNG
-console.log(makeCase("this is a string", ["upper", "snake"])); // THIS_IS_A_STRING
+// console.log(makeCase("this is a string", "pascal")); // ThisIsAString
+// console.log(makeCase("this is a string", "snake")); // this_is_a_string
+// console.log(makeCase("this is a string", "kebab")); // this-is-a-string
+// console.log(makeCase("this is a string", "title")); // This Is A String
+// console.log(makeCase("this is a string", "vowel")); // thIs Is A strIng
+// console.log(makeCase("this is a string", "consonant")); // THiS iS a STRiNG
+// console.log(makeCase("this is a string", ["upper", "snake"])); // THIS_IS_A_STRING
 
 
 
---
+++ caseMakerII-16-v2.js
@@ -0,0 +1,78 @@
+/* Precedence of each of the casing styles are as follows, values higher in the list should be processed first:
+
+1. camel, pascal, snake, kebab, title
+2. vowel, consonant
+3. upper, lower
+
+Our function should be able to handle all of these cases. */
+
+
+
+
+const makeCase = function (input, cases) {
+
+  let processed = [];
+  let index = 0;
+
+  const whitespace = ' ';
+  const vowel = 'aeiou';
+  const skipSeparators = ' -_';
+
+  const precedence = [
+    ['camel', 'pascal', 'snake', 'kebab', 'title'],
+    ['vowel', 'consonant'],
+    ['upper', 'lower']
+  ];
+
+  const toggle = {
+    kebab: '-',
+    snake: '_',
+    title: ' ',
+    precedence1: '',
+    precedence2: '',
+    precedence3: '',
+  }
+
+  // set precedence -- stored in toggle object
+  for (let i = 0; i < cases.length; i++) {
+    if (precedence[0].indexOf(cases[i]) > -1) toggle.precedence1 = cases[i];
+    else if (precedence[1].indexOf(cases[i]) > -1) toggle.precedence2 = cases[i];
+    else toggle.precedence3 = cases[i];
+  }
+
+  // first pass, first precedence
+  do {
+    // first letter upper
+    if (toggle.precedence1 === 'title' || toggle.precedence1 === 'pascal') {
+      processed[0] = input[0].toUppercase();
+      index = 1;
+    }
+  } while (index < input.length) {
+
+    // no whitespace
+    if (toggle.precedence1 === 'camel' || toggle.precedence1 === 'pascal') {
+      if (input[index] === whitespace) {
+        processed[index] = input[index + 1].toUppercase();
+        index++
+      } else {
+        processed[index] = input[index];
+      }
+
+
+    } else if (toggle.precedence1 ===) { // the newxt first precedence
+
+    }
+
+
+
+    // do the next two loops as ternary
+
+
+
+
+
+
+
+    index++
+  }
+}
