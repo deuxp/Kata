2b584241b68bd540da7ecc2329506d831814b3b8
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-10-12 19:53:58 -0400

    finished Kata 18 Square Code

--- Square-Code-18.js
+++ Square-Code-18.js
@@ -1,38 +1,34 @@
 const squareCode = function (message) {
+  // no whitespace;
   message = message.split('');
   message = message.filter(character => character !== ' ');
   message = message.join('');
+  // partitions: working square and crypted square
   let square = [];
   let cryptedSquare = [];
-  const columnLength = Math.ceil(Math.sqrt(message.length))
-  let squareTEMP = [];
 
-  // console.log(message);
+  const columnLength = Math.ceil(Math.sqrt(message.length));
+
+  // take off each column length until string is empty
   while (message.length > 1) {
     let a = message.substring(0, columnLength);
     let b = message.substring(columnLength);
     square.push(a);
     message = b
-    // console.log(b);
   };
-
-  // console.log(square);
-  // console.log(square[0][0]);
-  // console.log(message[0][0]);
   for (let x = 0; x < columnLength; x++) {
     for (let y = 0; y < square.length; y++) {
-      console.log(square[y][x]);
+      cryptedSquare.push(square[y][x]);
     }
-    console.log(' ');
+    cryptedSquare.push(' ');
   }
-
-  // return squareTEMP
+  return cryptedSquare.join('');
 };
 
 console.log(squareCode("chill out"));  // clu hlt io  
-// console.log(squareCode("feed the dog"));  // fto ehg ee dd
-// console.log(squareCode("have a nice day"));  // hae and via ecy
-// console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));  // imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
+console.log(squareCode("feed the dog"));  // fto ehg ee dd
+console.log(squareCode("have a nice day"));  // hae and via ecy
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));  // imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
 
 
-// 25 + 25 + 12
+// 25 + 25 + 15 + 6 minutes
